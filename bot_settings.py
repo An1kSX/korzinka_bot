b022efa503bf39178a44237c0b1f89393b963c96
An1kSX <70918749+An1kSX@users.noreply.github.com>
2023-07-26 20:55:45 +0500

    Add files via upload

---
+++ bot_settings.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+
+
+
+
+bot_token = ''                                                           # Токен бота
+api_id = 0                                                               # api id из my.telegram.org
+api_hash = ''                                                            # api hash из my.telegram.org
+url_request = 'http://api.korzinka.88dev.net'                            # АПИ 
+
+
+bot = Client('KorzinkaBot', api_id, api_hash, bot_token = bot_token)
+
