c5ab7d03b5f5fd62165df53794818585915346d9
An1kSX <70918749+An1kSX@users.noreply.github.com>
2023-08-07 16:17:19 +0500

    Update bot_settings.py

--- bot_settings.py
+++ bot_settings.py
@@ -3,9 +3,9 @@ from pyrogram import Client
 
 
 
-bot_token = '5876414656:AAEN_561Dt7wuX3HtNjcDkemP5s7VoUobaA'                                                           # Токен бота
-api_id = 1472978                                                               # api id из my.telegram.org
-api_hash = '54ad916d47750d25ac20ebfc80b4efe4'                                                            # api hash из my.telegram.org
+bot_token = ''                                                           # Токен бота
+api_id = 0                                                               # api id из my.telegram.org
+api_hash = ''                                                            # api hash из my.telegram.org
 url_request = 'http://api.korzinka.88dev.net'                            # АПИ 
 
 
